2a298a8c22bea68a91f116b063f9c3ed01df2b6d
MrFrankCin <federico.cinus@gmail.com>
2019-05-22 19:51:02 +0200

    forcing to upload data

---
+++ README.md
@@ -0,0 +1,176 @@
+# WoMG: Word of Mouth Generator
+
+This repository provides a reference implementation of *WoMG* as described in the thesis:<br>
+> WoMG: A synthetic Word-of-Mouth cascades Generator.<br>
+> Federico Cinus.<br>
+> M.Sc. Physics of Complex Systems, Università degli studi di Torino, Turin, 2017-2018.<br>
+> <Insert paper link>
+
+The *WoMG* software generates synthetic datasets of documents cascades on network. 
+It starts with any (un)directed, (un)weighted graph and a collection of documents and it outputs the propagation DAGs of the docs through the network. 
+Diffusion process is guided by the nodes underlying preferences. Please check the [project page]() for more details. 
+
+## Basic Usage
+
+### Example
+WoMG can be used both in command line and Jupyter notebook; check out the tutorial notebook in the **example** folder. 
+To run *WoMG* on a **demo** mode, execute the following command from the src directory:<br/>
+
+``python womg``
+
+It will generates 100 documents to be spread over the default network (*Les Miserables* http://konect.uni-koblenz.de/networks/moreno_lesmis).
+
+### Options
+You can check out the other options available to use with *WoMG* using:<br/>
+
+``python womg --help``
+
+### Input
+1. [Network] The supported input format is an edgelist (txt extension):
+	
+		node1_id_int node2_id_int <weight_float, optional>
+		
+You can specify the edgelist path using the *graph* argument:
+
+``python womg --graph /this/is/an/example/path/Graph_Folder/edgelist.txt``
+
+If no path is given the default network is *Les Miserables* network.
+
+2. [Documents] The supported input format for documents collection (corpus) is txt. You have to specify the folder path containing them using the *docs_folder* argument:
+
+ ``python womg --docs_folder /this/is/an/example/path/Corpus_Folder``
+ 
+If no documents folder path is given, *WoMG* will be set to generative mode.
+  
+### Output
+Diffusion output file is in a dict format: 
+
+	{time: {doc: [activating nodes]}}
+
+You can specify the output folder path:
+
+  ``python womg --output /this/is/an/example/path/Output_Folder``
+
+
+
+
+## Usage for researchers
+*WoMG* is an open source reasearch project. More details of the software are reported below:
+
+### Input
+1. [Network] The supported input format is an edgelist (txt extension):
+	
+		node1_id_int node2_id_int <weight_float, optional>
+		
+The graph is assumed to be undirected and unweighted by default. These options can be changed by setting the appropriate flags. You can specify the edgelist path using the *graph* argument):
+
+``python womg --graph /this/is/an/example/path/Graph_Folder/edgelist.txt``
+
+If no path is given the default network is *Les Miserables* network.
+
+2. [Documents] The supported input format for documents collection (corpus) is txt. You have to specify the folder path containing them using the *docs_folder* argument:
+
+ ``python womg --docs_folder /this/is/an/example/path/Corpus_Folder``
+
+If no documents folder path is given, WoMG will be set to generative mode.
+  
+### Output
+There are outputs for each class (or model)
+
+1. [Diffusion] file could be in two formats:
+
+  list (default): 
+  
+	time doc activating_node
+  dict : 
+  
+  	{ time: { doc: [activating nodes] } }
+
+2. [Network] files:
+  [info] dict: 
+  
+	{'type': 'Graph', 'numb_nodes': '77', 'numb_edges': '254', 'aver_degree': '6.5974', 'directed': 'False'} 
+	
+  [graph] dict: 
+  
+      {(u, v): [1.3, 0.2, 0.8, ... , 0.91], ...}
+      
+  Key: link-tuple. Value: weight vector
+
+  [interests and influence vectors] dict:
+  
+	{(node, 'int'): [interest vector], (node, 'inlf'): [influence vector]}
+
+3. [Topic] files:
+
+  [topic distributions] dict:
+  
+      {doc: [topic distribution]}
+      
+  [viralities] dict:
+  
+      {doc: virality}
+
+One can modify the outputs formats extension with the *format* argument:
+
+  ``python womg --format pickle``
+  ``python womg --format txt``
+  
+and specify the output folder path:
+
+  ``python womg --output /this/is/an/example/path/Output_Folder``
+
+
+### Options
+
+0. ``topics`` number of topics to be considered in the topic distributions of documents and nodes interests; it has to be less than number of dimensions of the nodes' space provided by node2vec
+##### Graph
+1. ``homophily`` H degree of homophily. Node2vec is used as baseline for generating interests vectors of the nodes starting from the given graph. Parameters *p* and *q* can achieve different decoded degree of homophily and structural equivalence (see paper). The best mix of them can be achieved only by a deep analysis of the network and a grid searh on the parameters. In order to pursuit generality in the input graph we use three degree of mixing: structural equivalence predominant, deepWalk (p=1, q=1), homophily predominant (which are not the best for representing the graph!).  1-H is the degree of social influence between nodes; which is the percentage of the avg interests vecs norms to be assigned to the influence vectors.
+
+##### Documents
+2. ``docs`` number of documents TO BE GENERATED by lda, giving this parameter lda will be directly set to generative mode
+3.  ``virality`` virality of the doc; if virality is high, exponent of the power law is high and threshold for activation is low.
+
+##### Diffusion
+4.  ``steps`` steps of the diffusion simulation
+5.  ``actives`` percentage of active nodes with respect to the total number of nodes in the intial configuration (before diffusion) for each doc.
+
+##### Node2Vec
+6.	``dimensions``        Number of dimensions for node2vec. Default 128
+7.	``walk-length``       length of walk per source. Default 80
+8.	``num-walks``       number of walks per source. Default 10
+9.	``window-size``      context size for optimization. Default 10
+10.	``iter``           number of epochs in SGD
+11.	``workers``     number of parallel workers. Default 8
+12.	``p``                manually set BFS parameter; else: it is set by H
+13.	``q``                 manually set DFS parameter; else: it is set by H
+
+##### Input and Output
+14.	``graph``       Input path of the graph edgelist
+15.	``weighted``            boolean specifying (un)weighted. Default unweighted
+16.	``unweighted``
+17.	``directed``            graph is (un)directed. Default undirected
+18.	``undirected``
+19.	``docs-folder``  Input path of the documents folder
+20.	``output``       Outputs path
+21.	``format``       Outputs format
+  
+22.	``seed``         Seed (int) for random distribution extraction
+
+
+### Citing
+
+
+	@inproceedings{,
+	author = {},
+	 title = {},
+	 booktitle = {Proceedings},
+	 year = {2019}
+	}
+
+
+### Miscellaneous
+
+Please feel free .. 
+
+*Note:* This is only a reference implementation analysis and more details are provided by the thesis.
---
+++ src/womg/network/tn.py
@@ -0,0 +1,370 @@
+# /network/tn.py
+# Implementation of the Topic-aware Network model
+import random
+import pathlib
+import collections
+import networkx as nx
+import numpy as np
+from tqdm import tqdm
+from sklearn.decomposition import NMF
+from node2vec_git.src.node2vec_main import node2vec_main
+from network.tlt_network_model import TLTNetworkModel
+from utils.utility_functions import read_edgelist
+
+
+
+class TN(TLTNetworkModel):
+    '''
+    Implementation of interest-influence model
+
+    Attributes
+    ----------
+    - interests_influence : dict
+      dictionary containing the influence and interests vectors
+      (of Hidden_numb_topics dimension) in the format:
+      key <- [node id, 'int'] (for interest) or [node id, 'infl'] (for influence)
+      value <- Hidden_numb_topics dimension array in numpy format
+    - Hidden_nx_obj : NetworkX object
+      networkx instance of the input network
+    - Hidden_godNode : dict
+      dictionary containing all the links of the god node that is out connected
+      to all the nodes but does not have in connections;
+      god node index (id) is -1; format will be:
+      key <- (-1, node id) [all int]
+      value <- link weight [int]
+    - Hidden_numb_topics : int
+      dimension of the interests and influence vectors
+
+    Methods
+    -------
+    - set_graph()
+    - set_godNode()
+    - set_interests()
+    - set_influence()
+    - node2interests(): generates realistic latent interests of the nodes starting
+      from the fixed given network
+    - graph_weights_vecs_generation(): updates network weights using interests
+      and influence vecs
+
+    Notes
+    -----
+    This class implementation inherits from the abstract class networkModel()
+
+    References
+    ----------
+
+    '''
+
+    def __init__(self, numb_topics, homophily, weighted, directed, path_in_graph=None, 
+                 god_node=True, method='node2interests', p=None,
+                 q=None, num_walks=None, walk_length=None, dimensions=None,
+                 window_size=None, workers=None, iiter=None):
+        super().__init__()
+        self.interests_influence = {}
+        self.Hidden_numb_topics = numb_topics
+        self.Hidden_homophily = homophily
+        self.Hidden_weighted = weighted
+        self.Hidden_directed = directed
+        self.Hidden_path_in_graph = path_in_graph
+        self.Hidden_godNode = {}
+        self.Hidden_god_node = god_node
+        self.Hidden_p = p 
+        self.Hidden_q = q 
+        self.Hidden_num_walks = num_walks
+        self.Hidden_walk_length = walk_length 
+        self.Hidden_dimensions = dimensions 
+        self.Hidden_window_size = window_size 
+        self.Hidden_workers = workers 
+        self.Hidden_iiter = iiter 
+        self.network_setup(method)
+        self.save_model_class()
+
+
+    def network_setup(self, method):
+        '''
+        - Sets the graph atribute using set_graph() method
+        - Sets the info attribute using set_info() method
+        - Sets the _godNode attribute using set_godNode() method
+        - Sets the interests vectors using set_interests() method
+        - Sets the influnce vecotrs using set_influence() mehtod
+        - Sets the new graph weights using update_weights() method
+
+        Notes
+        -----
+        See each method docstring for details
+        '''
+        if self.Hidden_path_in_graph == None:
+            print('No graph path provided \n DEMO Mode: generating cascades in les miserables network')
+            self.Hidden_path_in_graph = pathlib.Path("../") / "data" / "graph" / "lesmiserables" / "lesmiserables_edgelist.txt"
+            self.Hidden_nx_obj = read_edgelist(self,path=self.Hidden_path_in_graph, weighted=False, directed=False)
+        else:
+            self.Hidden_path_in_graph = pathlib.Path(self.Hidden_path_in_graph)
+            self.Hidden_nx_obj = read_edgelist(self, path=self.Hidden_path_in_graph, weighted=self.Hidden_weighted, directed=self.Hidden_directed)
+        self.set_graph()
+
+        if self.Hidden_god_node:
+            self.set_godNode()
+        self.set_interests(method)
+        self.set_influence()
+        #print('updating weights')
+        self.graph_weights_vecs_generation()
+
+
+    def set_graph(self):
+        '''
+        Sets the graph attribute formatting the networkx instance with gformat()
+        method of the superclass
+        '''
+        if isinstance(self.Hidden_nx_obj, nx.classes.graph.Graph):
+            self.graph = self.gformat(self.Hidden_nx_obj, directed=self.Hidden_directed)
+            self.set_info()
+        else:
+            print('Not a networkx readable object')
+
+
+    def set_info(self):
+        '''
+        Sets graph info dictionary attribute using networkx graph-instance description
+        '''
+        infos = nx.info(self.Hidden_nx_obj) + '\nDirected: '+str(nx.is_directed(self.Hidden_nx_obj))
+        infos = infos.split()
+        self.info['type'] = infos[2]
+        self.info['numb_nodes'] = infos[6]
+        self.Hidden_numb_nodes = int(infos[6])
+        self.info['numb_edges'] = infos[10]
+        if infos[2] == 'MultiDiGraph':
+            self.info['aver_in_degree'] = infos[14]
+            self.info['aver_out_degree'] = infos[18]
+            self.info['directed'] = infos[20]
+        else:
+            self.info['aver_degree'] = infos[13]
+            self.info['directed'] = infos[15]
+
+
+
+    def set_godNode(self, weight=1, nodes=None):
+        '''
+        Sets the godNode's links weights in the graph
+
+        Parameters
+        ----------
+        weight : int
+            scalar weight for the link/links
+            (Default 1)
+
+        nodes : iterable/int
+            nodes indexes or node index on which one wants to change link weights
+            if None is given all link weights (from godNode to each node) is changed
+            (Defalut None)
+
+        Example:
+        tn_instance.set_godNode() :
+        all the links weights (from godNode to each node) are set to 1
+        '''
+        if nodes is None:
+            print('Setting god node')
+            for node in tqdm(self.Hidden_nx_obj.nodes()):
+                self.Hidden_godNode[(-1, node)] = weight
+                self.graph.update(self.Hidden_godNode)
+        if isinstance(nodes, int):
+            self.Hidden_godNode[(-1, nodes)] = weight
+            self.graph.update(self.Hidden_godNode)
+        if isinstance(nodes, collections.Iterable):
+            for node in nodes:
+                self.Hidden_godNode[(-1, node)] = weight
+                self.graph.update(self.Hidden_godNode)
+
+    def set_interests(self, method='node2interests'):
+        '''
+        Creates interests vectors (numb_topics dimension) for each node and
+        save them in the interests_influence class attribute.
+
+        Parameters
+        ----------
+        - method : string
+          name of the method for creating interests vectors
+        '''
+        print('Generating interests:')
+        if method == 'node2interests':
+            self.node2interests(norm=False)
+        if method == 'random':
+            self.random_interests()
+
+
+    def set_influence(self, method='node2influence'):
+        '''
+        Sets influence vectors for all nodes
+
+        Parameters
+        ----------
+        - method : string
+          name of the method for creating interests vectors
+        '''
+        # rescaling infleunce importance
+        norm_avg = 0.
+        for node in self.Hidden_nx_obj.nodes():
+            norm_avg += np.linalg.norm(self.interests_influence[int(node), 'int'])/self.Hidden_numb_nodes
+        scale_fact = (1-self.Hidden_homophily)*norm_avg
+        # setting influence vec
+        for node in self.Hidden_nx_obj.nodes():
+            if method == 'node2influence':
+                influence_vec = self.node2influence(scale_fact)
+                self.interests_influence[node, 'infl'] = influence_vec
+
+    def node2interests(self, transl=True, norm=False):
+        '''
+        Create interests vector for each node using node2vec algorithm and
+        directly saves interests vectors in attribute 'interests_influence'.
+
+        3 steps:
+            1. finding node2vec embeddings
+            2. translation to positive axes of the embeddings
+            3. reduction with NMF to Hidden_numb_topics dimensions
+
+        Parameters
+        ----------
+        - method : string
+          'node2interests' uses the algorithm based on node2vec
+          (see node2interests() method)
+        - transl : bool
+          choose if interests have to be translated to positive axes
+        - norm : bool
+          choose if interests have to be normalize (True) or not (False)
+
+
+        Notes
+        -----
+        - Arrays are translated in order to have non-negative entries
+        - Dimension is reduced to the number of topics using NMF, which mantains
+          positivity
+        - Interests are the embeddings of the node2vec algorithm whose number
+          of features (or dimensions) is equal to the number of topics (Hidden_numb_topics)
+        - Method arg exists for a generalization aim: other method can be implemented
+        - p is fixed to 1, while q ranges from 0.25 (max homophily) to 4 (min homophily)
+        '''
+        # Final matrix
+        M = []
+        # Node2Vec
+
+        if self.Hidden_homophily <= 0.5:
+            self.Hidden_q = -6*self.Hidden_homophily + 4
+        else:
+            self.Hidden_q = -3./2*self.Hidden_homophily + 7./4
+        self.Hidden_p = 1
+        interests_model = node2vec_main(weighted=self.Hidden_weighted, 
+                                        graph=self.Hidden_path_in_graph,
+                                        directed=self.Hidden_directed,
+                                        p=self.Hidden_p, q=self.Hidden_q, 
+                                        num_walks=self.Hidden_num_walks, 
+                                        walk_length=self.Hidden_walk_length,
+                                        dimensions=self.Hidden_dimensions, 
+                                        window_size=self.Hidden_window_size, 
+                                        workers=self.Hidden_workers, 
+                                        iiter=self.Hidden_iiter)
+        #interests_model = node2vec.fit(window=10, min_count=1)
+
+        # Translation
+        if transl:
+            # translation constant
+            minim = 0.
+            for i in interests_model.wv.vocab:
+                if min(interests_model.wv[str(i)]) < minim:
+                    minim = min(interests_model.wv[str(i)])
+            ##
+            print("Computing interest vectors: ")
+            for node in tqdm(sorted(interests_model.wv.vocab)):
+                self.interests_influence[int(node), 'int'] = np.array([])
+                for topic in range(self.Hidden_numb_topics):
+                    self.interests_influence[int(node), 'int'] = np.append(self.interests_influence[int(node), 'int'],
+                                                                      interests_model.wv[node][topic] + abs(minim))
+                # Normalization
+                if norm:
+                    self.interests_influence[int(node), 'int'] = self.interests_influence[int(node), 'int'] / self.interests_influence[int(node), 'int'].sum()
+                M.append(self.interests_influence[int(node), 'int'])
+        # NO Translation
+        else:
+            for node in sorted(interests_model.wv.vocab):
+                self.interests_influence[int(node), 'int'] = np.array([])
+                for topic in range(self.Hidden_numb_topics):
+                    self.interests_influence[int(node), 'int'] = np.append(self.interests_influence[int(node), 'int'],
+                                                                  interests_model.wv[node][topic])
+                M.append(self.interests_influence[int(node), 'int'])
+
+        # NMF Reduction
+        print('Reducing dimensions from ', self.Hidden_dimensions,' to ', self.Hidden_numb_topics)
+        nmf = NMF(n_components=self.Hidden_numb_topics, random_state=42, max_iter=1000)
+        right = nmf.fit(M).components_
+        left = nmf.transform(M)
+        for node, index in zip(sorted(interests_model.wv.vocab), range(self.Hidden_numb_nodes)):
+            self.interests_influence[int(node), 'int'] = left[int(index)]
+            #print(self.interests_influence[int(node), 'int'])
+
+
+    def random_interests(self, norm=True):
+        '''
+        Create interests vector for each node using a random uniform probability density 
+        function and directly saves interests vectors in attribute 'interests_influence'
+        
+        Parameters
+        ----------
+        - norm : bool
+        if True interests vectors are normalized
+
+        '''
+        if norm:
+            for node in range(self.Hidden_numb_nodes):
+                self.interests_influence[int(node), 'int'] = np.random.rand(self.Hidden_numb_topics)
+
+
+    def node2influence(self, scale_fact, alpha_max=10):
+        '''
+        Creates influence vectors (numb_topics dimension) from dirichlet
+        distribution over topics for a single node
+
+        Parameters
+        ----------
+        - alpha_max : int
+          highest value in the dirichlet weight vec for a random entry
+        '''
+        dirich_weight_vec = [1 for topic in range(self.Hidden_numb_topics)]
+        dirich_weight_vec[random.randint(0, self.Hidden_numb_topics-1)] = alpha_max
+        influence_vec = np.random.dirichlet(dirich_weight_vec)
+        return scale_fact*influence_vec
+
+
+    def graph_weights_vecs_generation(self, god_node_weight=7):
+        '''
+        Creates weights vectors (numb_topcis dimension) for each direct link
+        and update the graph attribute; a link weight is defined as:
+
+        w_(i,j) = intersts_j + influence_i
+
+        Parameters
+        ----------
+        - god_node_weight : int
+          scalar value for each entry of the weight vector involving the godNode
+        '''
+        for link in self.graph.keys():
+            # god node
+            if link[0] == -1:
+                out_influence_vec = np.array([god_node_weight for i in range(self.Hidden_numb_topics)])
+                self.set_link_weight(link, out_influence_vec)
+            else:
+                out_influence_vec = self.interests_influence[(link[0], 'infl')]
+                in_interest_vec = self.interests_influence[(link[1], 'int')]
+                self.set_link_weight(link, out_influence_vec + in_interest_vec)
+
+
+    def set_link_weight(self, link, new_weight):
+        '''
+        Sets the link attribute to the given weights vector arg
+
+        Parameters
+        ----------
+        - link : tuple
+          link in the graph in which you want to change the attribute
+        - new_weight : array
+          numb_topic dimension array that is going to be the new attribute of the
+          link
+        '''
+        self.graph[link] = new_weight
