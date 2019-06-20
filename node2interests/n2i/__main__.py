16f2bfccdad0fd2195876f5b6a22cefb23fe9884
MrFrankCin <federico.cinus@gmail.com>
2019-06-20 15:18:36 +0200

    introducing node2interests package

---
+++ node2interests/n2i/__main__.py
@@ -0,0 +1,249 @@
+'''
+Reference implementation of node2vec.
+
+Author: Aditya Grover
+
+For more details, refer to the paper:
+node2vec: Scalable Feature Learning for Networks
+Aditya Grover and Jure Leskovec
+Knowledge Discovery and Data Mining (KDD), 2016
+'''
+import sys
+import click
+import pathlib
+import random
+import numpy as np
+import networkx as nx
+from tqdm import tqdm
+from sklearn.decomposition import NMF
+from n2i.node2vec import node2vec, read_graph, save_emb
+
+curr_path = str(pathlib.Path.cwd())
+
+def n2i_main(topics=15,
+         graph=None, fast=False,
+         weighted=False, directed=False,
+         output=curr_path, seed=None,
+         dimensions=128, walk_length=80,
+         num_walks=10, window_size=10,
+         iiter=1, workers=8,
+         p=1, q=1,
+         normalize=False,
+         translate=True,
+         reduce=True):
+    '''
+
+
+
+    Creates interests vector for each node using node2vec algorithm and
+    saves interests in a file.
+
+    3 steps:
+        1. finding node2vec embeddings
+        2. translation to positive axes of the embeddings
+        3. reduction with NMF to _topics dimensions
+
+    Parameters
+    ----------
+    - topics : int
+        Number of topics in the topic model. Default 15. K<d
+    - graph : str
+        Input path of the graph edgelist
+    - fast : bool
+        defines the method for generating nodes' interests. Two choices: 'node2interests', 'random'. Default setting is False -> 'node2interests
+    - weighted : bool
+        boolean specifying (un)weighted. Default  unweighted
+    - directed : bool
+        graph is (un)directed. Default  undirected
+    - output : str
+        Outputs path
+    - seed : int
+        Seed (int) for random distribution extractions
+    - dimensions : int
+        Number of dimensions for node2vec. Default 128
+    - walk_length : int
+        ength of walk per source. Default 80
+    - num_walks : int
+        number of walks per source. Default 10
+    - window_size : int
+        context size for optimization. Default 10
+    - iiter : int
+        number of epochs in SGD
+    - workers : int
+        number of parallel workers. Default   8
+    - p : float
+        manually set BFS parameter
+    - q : float
+        manually set DFS parameter
+    - norm : bool
+      choose if interests have to be normalize (True) or not (False)
+    - transl : bool
+      choose if interests have to be translated to positive axes
+    - reduce : bool
+        if True reduces dimensions with NMF
+
+
+
+    Notes
+    -----
+    - Arrays are translated in order to have non-negative entries
+    - Dimension is reduced to the number of topics using NMF, which mantains
+      positivity
+    '''
+    # seed
+    if seed != None:
+        random.seed(seed)
+        np.random.seed(seed)
+    if topics >= dimensions:
+        print('Topics have to be less than dimensions')
+        sys.exit()
+    # Embeddings
+    emb = {}
+    # Final matrix
+    M = []
+    # Node2Vec
+    #print(graph)
+    nx_graph = read_graph(weighted=weighted, graph=graph, directed=directed)
+    numb_nodes = nx.number_of_nodes(nx_graph)
+    interests_model = node2vec(weighted=weighted,
+                                graph=graph,
+                                directed=directed,
+                                p=p, q=q,
+                                num_walks=num_walks,
+                                walk_length=walk_length,
+                                dimensions=dimensions,
+                                window_size=window_size,
+                                workers=workers,
+                                iiter=iiter)
+
+    if reduce:
+    # Translation
+        if translate:
+            # translation constant
+            minim = 0.
+            for i in interests_model.wv.vocab:
+                if min(interests_model.wv[str(i)]) < minim:
+                    minim = min(interests_model.wv[str(i)])
+            ##
+            print("Computing interest vectors: ")
+            for node in tqdm(sorted(interests_model.wv.vocab)):
+                emb[int(node)] = np.array([])
+                for topic in range(topics):
+                    emb[int(node)] = np.append(emb[int(node)], interests_model.wv[node][topic] + abs(minim))
+                # Normalization
+                if normalize:
+                    emb[int(node)] = emb[int(node)] / emb[int(node)].sum()
+                M.append(emb[int(node)])
+        # NO Translation
+        else:
+            for node in sorted(interests_model.wv.vocab):
+                emb[int(node)] = np.array([])
+                for topic in range(topics):
+                    emb[int(node)] = np.append(emb[int(node)], interests_model.wv[node][topic])
+                M.append(emb[int(node)])
+
+        # NMF Reduction
+        print('Reducing dimensions from ', dimensions,' to ', topics)
+        nmf = NMF(n_components=topics, random_state=42, max_iter=1000)
+        right = nmf.fit(M).components_
+        left = nmf.transform(M)
+        for node, index in zip(sorted(interests_model.wv.vocab), range(numb_nodes)):
+            emb[int(node)] = left[int(index)]
+            #print(emb[int(node)])
+    else:
+        for node in interests_model.wv.vocab:
+            emb[node] = interests_model.wv[str(node)]
+
+    save_emb(emb=emb, path=output)
+
+
+@click.command()
+@click.option('--topics', metavar='K', default=15,
+                    help='Number of topics in the topic model. Default 15. K<d ',
+                    type=int)
+@click.option('--graph', default=None,
+                    help='Input path of the graph edgelist', type=str)
+
+@click.option('--fast', is_flag=True,
+                    help="defines the method for generating nodes' interests. Two choices: 'node2interests', 'random'. Default setting is False -> 'node2interests",
+                    default=False)
+
+@click.option('--weighted', is_flag=True,
+                    help='boolean specifying (un)weighted. Default  unweighted', default=False)
+
+@click.option('--directed', is_flag=True,
+                    help='graph is (un)directed. Default  undirected',
+                    default=False)
+@click.option('--output', default=curr_path, help='Outputs path')
+@click.option('--seed', help='Seed (int) for random distribution extractions',
+                    type=int, required=False)
+
+
+@click.option('--dimensions', metavar='d', type=int, default=128,
+                    help='Number of dimensions for node2vec. Default 128')
+
+@click.option('--walk_length', metavar='w', type=int, default=80,
+                    help='length of walk per source. Default 80')
+
+@click.option('--num_walks', metavar='nw', type=int, default=10,
+                    help='number of walks per source. Default 10')
+
+@click.option('--window_size', metavar='ws', type=int, default=10,
+                    help='context size for optimization. Default 10')
+
+@click.option('--iiter', default=1, type=int,
+                  help='number of epochs in SGD. Default 1')
+
+@click.option('--workers', type=int, default=8,
+                    help='number of parallel workers. Default   8')
+
+@click.option('--p', type=float, default=1,
+                    help='manually set BFS parameter')
+
+@click.option('--q', type=float, default=1,
+                    help='manually set DFS parameter')
+@click.option('--normalize', is_flag=True,
+                    help='normalize embeddings',
+                    default=False)
+@click.option('--translate', is_flag=True,
+                    help='translate embeddings to potsitive axes',
+                    default=True)
+@click.option('--reduce', is_flag=True,
+                    help='reduce dimension with NMF',
+                    default=True)
+def main_cli(topics,
+         graph, fast,
+         weighted, directed,
+         output, seed,
+         dimensions, walk_length,
+         num_walks, window_size,
+         iiter, workers,
+         p, q,
+         normalize,
+         translate,
+         reduce):
+    '''
+
+
+    Creates interests vector for each node using node2vec algorithm and
+    saves interests in a file.
+
+    3 steps:
+        1. finding node2vec embeddings
+        2. translation to positive axes of the embeddings
+        3. reduction with NMF to _topics dimensions
+    '''
+    n2i_main(topics=topics,
+             graph=graph, fast=fast,
+             weighted=weighted, directed=directed,
+             output=output, seed=seed,
+             dimensions=dimensions, walk_length=walk_length,
+             num_walks=num_walks, window_size=window_size,
+             iiter=iiter, workers=workers,
+             p=p, q=q,
+             normalize=normalize,
+             translate=translate,
+             reduce=reduce)
+
+if __name__ == '__main__':
+    main_cli()
---
+++ node2interests/n2i/node2vec.py
@@ -0,0 +1,99 @@
+'''
+Reference implementation of node2vec.
+
+Author: Aditya Grover
+
+For more details, refer to the paper:
+node2vec: Scalable Feature Learning for Networks
+Aditya Grover and Jure Leskovec
+Knowledge Discovery and Data Mining (KDD), 2016
+'''
+import pathlib
+import networkx as nx
+from n2i.graph import Graph
+from gensim.models import Word2Vec
+
+def read_graph(weighted, graph, directed):
+    '''
+    Reads the input network in networkx.
+    '''
+    #print(graph)
+    if weighted:
+        G = nx.read_edgelist(graph, nodetype=int, data=(('weight',float),), create_using=nx.DiGraph())
+    else:
+        G = nx.read_edgelist(graph, nodetype=int, create_using=nx.DiGraph())
+        for edge in G.edges():
+            G[edge[0]][edge[1]]['weight'] = 1
+
+    if not directed:
+        G = G.to_undirected()
+
+    return G
+
+def learn_embeddings(walks, dimensions, window_size, workers, iiter):
+    '''
+    Learn embeddings by optimizing the Skipgram objective using SGD.
+    '''
+    walks = [list(map(str, walk)) for walk in walks]
+    model = Word2Vec(walks, size=dimensions, window=window_size, min_count=0,
+                     sg=1, workers=workers, iter=iiter)
+    #model.wv.save_word2vec_format(args.output)
+
+    return model.wv
+
+def node2vec(weighted, graph, directed, p, q, num_walks, walk_length,
+                  dimensions, window_size, workers, iiter):
+    '''
+    Pipeline for representational learning for all nodes in a graph.
+    '''
+    nx_G = read_graph(weighted, graph, directed)
+    G = Graph(nx_G, directed, p, q)
+    G.preprocess_transition_probs()
+    walks = G.simulate_walks(num_walks, walk_length)
+    emb_model = learn_embeddings(walks, dimensions, window_size,
+                                 workers, iiter)
+
+    return emb_model
+
+def save_emb(emb, path):
+    '''
+    saves embeddings in path with the following format :
+    node_id [embeddings]
+    '''
+    file_name = make_filename('emb', pathlib.Path(path))
+    with open(file_name, 'w') as f:
+        for node in emb.keys():
+            tmp_emb = [_ for _ in emb[node]]
+            f.write(str(node) + ' ' + str(tmp_emb) + '\n')
+    print('Embeddings have been saved in ', path)
+
+def make_filename(name, output_dir, new_file=True):
+    '''
+    Returns the correct filename checking the file "name" is in "output_dir"
+    If the file already exists it returns a filename with an incrementing
+    index.
+
+    Parameters
+    ----------
+    - name : str
+        name of the file
+    - output_dir : str
+        path of the "name" file
+
+    Returns
+    -------
+    Filename (str)
+    '''
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    filename = output_dir / str(name + "0.txt")
+    sim_numb = 0
+
+    while pathlib.Path(filename).exists():
+        sim_numb+=1
+        filename = output_dir / str(name + str(sim_numb) + ".txt")
+    if new_file:
+        return filename
+    else:
+        sim_numb-=1
+        filename = output_dir / str(name + str(sim_numb) + ".txt")
+        return filename
