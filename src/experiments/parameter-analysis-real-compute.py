df935ebdcff1259d2fd01c6678e90f62258f276e
MrFrankCin <federico.cinus@gmail.com>
2019-12-09 18:16:53 +0100

    EXPERIMENTS SCRIPT

--- src/experiments/parameter-analysis-real-compute.py
+++ src/experiments/parameter-analysis-real-compute.py
@@ -3,23 +3,25 @@ import datetime
 import pandas as pd
 import os
 from womg_core.__main__ import womg_main
-import multiprocessing
+import multiprocessing.pool
 import numpy as np
 import uuid
 import shutil
 
-N_JOBS = 8
+# N_JOBS = 4
 N_TOPICS = 10
 N_DOCS = 3553 # Number of items is the same in reduced and not reduced Digg
 N_STEPS = 1000
 god_node_strength = 0
-int_mode = 'nmf'
-graph_path = '../womg_core/womgdata/graph/digg/graph_filtered.csv'
+int_mode = 'nmf'   
+# graph_path = '/home/fcinus/.local/lib/python3.6/site-packages/womgdata/graph/lesmiserables/lesmiserables_edgelist.txt'
+graph_path = '/home/fcinus/.local/lib/python3.6/site-packages/womgdata/graph/digg/graph_filtered.csv'
+
 #graph_path = '/home/corradom/projects/WoMG/src/womg/high-clustered-sf.nx'
 assert os.path.exists(graph_path)
 # graph_path = '../data/graph/barabasi/barabasi_edgelist.txt'
 directed = True
-nr_experiments = 4
+nr_experiments = 4 
 
 single_activators = (True, )  # Digg is not single activator
 infl_strengths = [None]
@@ -32,40 +34,42 @@ simulat_path = 'digg-by-int-sim' + timestamp + '/'
 
 def run_one_experiment(arguments):
     seed, single_activator, infl_strength, h, v = arguments
-
+    
     # Create output folder
     path = simulat_path + str(uuid.uuid4())
     os.mkdir(path)
-
+    
     # Run experiments
     womg_main(path_out=path, graph_path=graph_path,
               directed=directed, int_mode=int_mode,
-              numb_topics=N_TOPICS, numb_docs=N_DOCS, numb_steps=N_STEPS,
+              numb_topics=N_TOPICS, numb_docs=N_DOCS, numb_steps=N_STEPS, 
               homophily=h, virality=v, gn_strength=god_node_strength,
               single_activator=single_activator,
               infl_strength=infl_strength,
               seed=seed)
-
+              
     # Read results
     file_prop = path + "/Propagations0.txt"
     df = pd.read_csv(file_prop, sep=' ', names=['time', 'item', 'node'])
+    if df.empty:
+    	print('\n Parameters h=', h, ' v=',v , ' infl_strength=', infl_strength, ' single_Activator=', single_activator, ' provided no cascade! \n')
     item_per_node = df.groupby('node').item.nunique().values
     node_per_item = df.groupby('item').node.nunique().values
     time_per_item = df.groupby('item').time.max().values + 1
-
+    
     # Write results
-    new_row = pd.DataFrame([[single_activator, infl_strength, h, v, seed,
+    new_row = pd.DataFrame([[single_activator, infl_strength, h, v, seed, 
         np.mean(item_per_node), np.mean(node_per_item), np.mean(time_per_item),
         item_per_node, node_per_item, time_per_item
     ]])
-    shutil.rmtree(path, ignore_errors=True)
+    #shutil.rmtree(path, ignore_errors=True)
     with open(results_path, 'a') as f:
         new_row.to_csv(f, header=False)
         f.flush()
 
 if __name__ == '__main__':
     os.mkdir(simulat_path)
-
+    
     with open(results_path, 'w') as f:
         f.write('single_activator,infl_strength,homophily,virality_exp,seed,'
                 'mean_item_per_node,mean_node_per_item,mean_max_time,'
@@ -77,7 +81,11 @@ if __name__ == '__main__':
             for infl_strength in infl_strengths:
                 for h in homophily:
                     for v in virality:
-                        args.append([seed, single_activator, infl_strength, h, v])
+                        # args.append([seed, single_activator, infl_strength, h, v])
+                        arg = [seed, single_activator, infl_strength, h, v]
+                        print(arg)
+                        run_one_experiment(arg)
+    
+    # pool = multiprocessing.pool.ThreadPool(N_JOBS)
+    # pool.map(run_one_experiment, args)
 
-    pool = multiprocessing.Pool(N_JOBS)
-    pool.map(run_one_experiment, args)
