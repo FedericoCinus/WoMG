01583541c3d5c30ec22420b2e2963019c6627f21
MrFrankCin <federico.cinus@gmail.com>
2020-01-10 16:27:21 +0100

    setup file

--- src/womg_core/setup.py
+++ src/womg_core/setup.py
@@ -24,7 +24,7 @@ def find_files(folder, rec=True):
 ###############################################################################
 setuptools.setup(
     name="womg-core",
-    version="0.9.16",
+    version="0.9.45",
     author="Federico Cinus",
     author_email="federico.cinus@isi.it",
     description="Word-of-Mouth cascades Generator",
